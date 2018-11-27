4d158aceaa2de41500c4670f5b416e01ed32fa0e
Jordan <jordanhaddadi@gmail.com>
2018-11-27 16:15:41 -0500

    added regexp to find searchterm in title and content

--- scratch/queries.js
+++ scratch/queries.js
@@ -5,15 +5,19 @@ const Note = require('../models/note');
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
   .then(() => {
-    const searchTerm = 'lady gaga';
-    let filter = {};
-
-    if (searchTerm) {
-      filter.title = { $regex: searchTerm, $options: 'i' };
-    }
-
-    return Note.find(filter).sort({ updatedAt: 'desc' });
+    const searchTerm = 'gaga';
+    const regex = new RegExp(searchTerm, 'i');
+    return Note.find(
+        {
+            $or: [
+                {title: regex},
+                {content: regex}
+            ]
+        }
+    )
+    // Note.sort({updatedAt: 'desc'});
   })
+  
   .then(results => {
     console.log(results);
   })
@@ -25,34 +29,92 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
     console.error(err);
   });
 
-  mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
-  .then(() => {
-    const id = '000000000000000000000001';
-    return Note.find({ _id: id });
-  })
-  .then(results => {
-    console.log(results);
-  })
-  .then(() => {
-    return mongoose.disconnect()
-  })
-  .catch(err => {
-    console.error(`ERROR: ${err.message}`);
-    console.error(err);
-  });
+//   mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+//   .then(() => {
+//     const id = '000000000000000000000001';
+//     return Note.find({ _id: id });
+//   })
+//   .then(results => {
+//     console.log(results);
+//   })
+//   .then(() => {
+//     return mongoose.disconnect()
+//   })
+//   .catch(err => {
+//     console.error(`ERROR: ${err.message}`);
+//     console.error(err);
+//   });
 
-  mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
-  .then(() => {
-    const id = '000000000000000000000001';
-    return Note.find({ _id: id });
-  })
-  .then(results => {
-    console.log(results);
-  })
-  .then(() => {
-    return mongoose.disconnect()
-  })
-  .catch(err => {
-    console.error(`ERROR: ${err.message}`);
-    console.error(err);
-  });
+//   mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+//   .then(() => {
+//     const id = '000000000000000000000001';
+//     return Note.findById(id);
+//   })
+//   .then(results => {
+//     console.log(results);
+//   })
+//   .then(() => {
+//     return mongoose.disconnect()
+//   })
+//   .catch(err => {
+//     console.error(`ERROR: ${err.message}`);
+//     console.error(err);
+//   });
+
+//   mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+//   .then(() => {
+//     const newNote = {
+//         title: "Where are all the puppies?????",
+//         content: "Lorem Ipsum"
+//     };
+//     return Note.create(newNote);
+//   })
+//   .then(results => {
+//     console.log(results);
+//   })
+//   .then(() => {
+//     return mongoose.disconnect()
+//   })
+//   .catch(err => {
+//     console.error(`ERROR: ${err.message}`);
+//     console.error(err);
+//   });
+
+//   mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+//   .then(() => {
+//     const id = '5bfda30c45d25714f36246f1';
+//     const newNote = {
+//         id: id,
+//         title: "I'M AN UPDATE NOTE YAYYYYY!!!!!!",
+//         content: "Lorem Ipsum"
+//     };
+//     return Note.findByIdAndUpdate(id, newNote);
+//   })
+//   .then(results => {
+//     console.log(results);
+//   })
+//   .then(() => {
+//     return mongoose.disconnect()
+//   })
+//   .catch(err => {
+//     console.error(`ERROR: ${err.message}`);
+//     console.error(err);
+//   });
+
+//   mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+//   .then(() => {
+//     const id = '5bfda30c45d25714f36246f1';
+//     return Note.findByIdAndRemove(id);
+//   })
+//   .then(results => {
+//     console.log(results);
+//   })
+//   .then(() => {
+//     return mongoose.disconnect()
+//   })
+//   .catch(err => {
+//     console.error(`ERROR: ${err.message}`);
+//     console.error(err);
+//   });
+
+
