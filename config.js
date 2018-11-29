fe294580558b65357bb8c4003f74ff1f57be59b3
Jordan <jordanhaddadi@gmail.com>
2018-11-29 15:06:01 -0500

    added folder model

--- config.js
+++ config.js
@@ -2,5 +2,6 @@
 
 module.exports = {
   PORT: process.env.PORT || 8080,
-  TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost:27017/noteful-test'
+  TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost:27017/noteful-test',
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/noteful',
 };
